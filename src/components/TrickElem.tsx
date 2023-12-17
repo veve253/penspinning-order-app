84cca106121c3c103b4988f8a85c2d7a3d2ea7c5
veve253 <tata37521@gmail.com>
2023-12-17 21:30:57 +0900

    trickのUIを変更

--- src/components/TrickElem.tsx
+++ src/components/TrickElem.tsx
@@ -13,21 +13,21 @@ const TrickElem: FC<{
     };
 
   return (
-    <div className="flex justify-between border mx-auto text-xs w-4/5 min-h-[32px] rounded-full">
+    <div className="flex justify-between border mx-auto text-xs w-4/5 min-h-[32px] rounded-xl">
       <div className="w-6 my-auto h-full  flex items-center justify-center">
         {index + 1}
       </div>
-      <div className="py-auto pl-4 flex items-center min-h-[32px] w-full border-r border-l">
-        <span className="my-auto hover:border-b cursor-pointer min-w-min">
+      <div className="py-auto pl-4 flex items-center min-h-[32px] w-full  border-l">
+        <span className="my-auto  w-full">
           <input
             type="text"
             value={name}
             onChange={handleUpdateTrick}
-            className="w-full"
+            className="w-full hover:underline underline-offset-4 cursor-pointer focus:ring-0 focus-visible:ring-0 focus-visible:outline-none"
           />
         </span>
       </div>
-      <button className="w-8 my-auto h-full text-[10px] flex items-center justify-center hover:bg-red-300 rounded-r-full">
+      <button className="w-8 p-[1px] my-auto mr-[1.5px] h-full text-[10px] flex items-center justify-center hover:bg-gray-200 rounded-full">
         削除
       </button>
     </div>
