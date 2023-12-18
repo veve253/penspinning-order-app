e117ebf81c63e8689d912010c3c7408a486f6acd
veve253 <tata37521@gmail.com>
2023-12-18 15:30:34 +0900

    trickのUIを変更

--- src/components/TrickElem.tsx
+++ src/components/TrickElem.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useFSContext } from "../contexts/FSContexts";
 import { Trick } from "../types/trickType";
 
@@ -9,6 +9,22 @@ const TrickElem: FC<{
 }> = ({ id, index, name }) => {
   const { deleteTrick, selectedFS, setSelectedFS, updateTrick } =
     useFSContext();
+
+  const [clicked, setClicked] = useState(false);
+
+  const inputTrickRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    clicked ? inputTrickRef.current?.focus() : inputTrickRef.current?.blur();
+  }, [clicked]);
+
+  const handleOnClick = async () => {
+    setClicked(true);
+  };
+
+  const handleOnBlur = () => {
+    setClicked(false);
+  };
 
   // trickの変更をUIに反映
   const handleUpdateTrick = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,19 +46,28 @@ const TrickElem: FC<{
 
   return (
     <div className="flex justify-between border mx-auto text-[16px] w-4/5 min-h-[32px] rounded-xl">
-      <div className="w-6 my-auto h-full  flex items-center justify-center">
+      <div className="w-6 my-auto h-full flex items-center justify-center">
         {index + 1}
       </div>
-      <div className="py-auto pl-4 flex items-center min-h-[32px] w-full  border-l">
-        <span className="my-auto flex  w-full">
-          <input
-            type="text"
-            value={name}
-            onChange={handleUpdateTrick}
-            onBlur={applyUpdatingTrick}
-            className="w-full focus:underline underline-offset-4 cursor-pointer focus:outline-none"
-          />
-        </span>
+      <div
+        onClick={handleOnClick}
+        onBlur={handleOnBlur}
+        className="py-auto pl-4 flex items-center min-h-[32px] w-full border-l break-words break-all"
+      >
+        {clicked ? (
+          <span className="my-auto flex w-full">
+            <input
+              type="text"
+              value={name}
+              ref={inputTrickRef}
+              onChange={handleUpdateTrick}
+              onBlur={applyUpdatingTrick}
+              className="w-full focus:underline underline-offset-4 cursor-pointer focus:outline-none"
+            />
+          </span>
+        ) : (
+          name
+        )}
       </div>
       <button
         onClick={handleDeleteTrick}
--- src/containers/FS.tsx
+++ src/containers/FS.tsx
@@ -1,27 +1,15 @@
 import "./FS.css";
 import TrickElem from "../components/TrickElem";
-import { FC } from "react";
 import { useFSContext } from "../contexts/FSContexts";
 
-const FS: FC<{
-  // trickList: Trick[];
-  // removeTrick: (index: number) => void;
-  // updateTrick: (index: number, newTrick: string) => void;
-}> = () => {
+const FS = () => {
   const { selectedFS } = useFSContext();
   return (
     <div className="mt-6">
       {selectedFS.map((trick: any, index: any) => {
         return (
           <div key={index}>
-            <TrickElem
-              id={trick.id}
-              name={trick.trick}
-              index={index}
-              // trickIndex={trick.index}
-              // removeTrick={removeTrick}
-              // updateTrick={updateTrick}
-            />
+            <TrickElem id={trick.id} name={trick.trick} index={index} />
             {index !== selectedFS.length - 1 && (
               <div className="text-center">
                 <div className="px-auto inline-block align-middle text-gray-400 leading-none w-2 h-2 mb-[1px] border border-current border-l-0 border-b-0 box-border custom-chevron"></div>
